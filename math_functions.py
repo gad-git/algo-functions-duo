4625c997d527e1548c23c191ede6d6eafbb35584
Haim Daniel Aflalo <kvim.hda@gmail.com>
2025-06-19 16:54:27 +0300

    Add factorial functions

--- math_functions.py
+++ math_functions.py
@@ -7,4 +7,10 @@ def welcome():
 
 if __name__ == '__main__':
     welcome()
-    
+
+def factorial(n):
+    if n <= 1:
+        return 1
+    return n * factorial(n-1)
+if __name__ == "__main__":
+    print(factorial(100))
