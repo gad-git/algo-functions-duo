a739df97409e6256ad24b73dae04b440ba13d9d6
Haim Daniel Aflalo <kvim.hda@gmail.com>
2025-06-19 19:31:30 +0300

    Add maths function

--- math_functions.py
+++ math_functions.py
@@ -5,10 +5,6 @@ def welcome():
     '''welcome function'''
     print('welcometo the algorithmic functions library!')
 
-if __name__ == '__main__':
-    welcome()
-<<<<<<< HEAD
-    
 def is_prime(n, i = 2):
     if n < 2:
         return False 
@@ -17,15 +13,14 @@ def is_prime(n, i = 2):
     if n % i == 0:
         return False
     return is_prime(n, i + 1)
-    
-
-    
-=======
 
 def factorial(n):
     if n <= 1:
         return 1
     return n * factorial(n-1)
+
+
 if __name__ == "__main__":
+    welcome()
     print(factorial(100))
->>>>>>> 4625c997d527e1548c23c191ede6d6eafbb35584
+
