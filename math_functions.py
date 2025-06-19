cdc3a097a5ba67f40357b09bc388cc3f88778ea4
Gad <gadvineblat@gmail.com>
2025-06-19 17:32:40 +0300

    add prime number checking function

--- math_functions.py
+++ math_functions.py
@@ -8,3 +8,14 @@ def welcome():
 if __name__ == '__main__':
     welcome()
     
+def is_prime(n, i = 2):
+    if n < 2:
+        return False 
+    if i * i > n:
+        return True
+    if n % i == 0:
+        return False
+    return is_prime(n, i + 1)
+    
+
+    
