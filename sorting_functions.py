70832092e5e4db41149c5da01e768b122da16f46
gad <gadvineblat@gmail.com>
2025-06-19 19:52:12 +0300

    dasdas

---
+++ sorting_functions.py
@@ -0,0 +1,18 @@
+def bubble_sort(lst):
+    for i in range(len(lst) - 1):
+        swapped = False
+        for j in range(len(lst) - 1 - i):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+                swapped = True
+        if not swapped:
+            break
+    return lst
+
+def quick_sort(lst):
+    if len(lst) <= 1:
+        return lst
+    pivot = lst[0]
+    less = [x for x in lst[1:] if x <= pivot]
+    greater = [x for x in lst[1:] if x > pivot]
+    return quick_sort(less) + [pivot] + quick_sort(greater)
