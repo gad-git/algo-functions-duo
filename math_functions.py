150fd44e4e336cf081a01efe64e281a3e0413ad1
Gad <gadvineblat@gmail.com>
2025-06-19 16:39:33 +0300

    Add initial file with welcome function

---
+++ math_functions.py
@@ -0,0 +1,10 @@
+'''library of algorithmic functions
+created by Gad and Haim'''
+
+def welcome():
+    '''welcome function'''
+    print('welcometo the algorithmic functions library!')
+
+if __name__ == '__main__':
+    welcome()
+    
